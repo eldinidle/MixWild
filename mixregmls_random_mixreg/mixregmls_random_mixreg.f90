10b4e62ffb2e0d5ff2e5fc8adcec1c20ecb07ed1
Rachel Nordgren <rknordgren@gmail.com>
2018-01-28 04:34:04 +0000

    mixregmls_random_mixreg.f90 edited online with Bitbucket

--- mixregmls_random_mixreg/mixregmls_random_mixreg.f90
+++ mixregmls_random_mixreg/mixregmls_random_mixreg.f90
@@ -1408,7 +1408,8 @@ ALLOCATE (TEMPR(NVAR))
      end if
         if(discard0 .ne. 0) WRITE(IUN,508)num0
         508 FORMAT(//,1x,'==> The number of level 2 observations removed because of non-varying responses =', I6)
-        if(discard0 .ne. 0 .and. num0 > 0) write(IUN,*) '(see '//trim(fileprefix)//'_removed.dat for information about those clusters)'
+        if(discard0 .ne. 0 .and. num0 > 0) &
+            write(IUN,*) '(see '//trim(fileprefix)//'_removed.dat for information about those clusters)'
 
         CLOSE(IUN)
     END SUBROUTINE PRINTDESC
