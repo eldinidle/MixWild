08aa70172dbe37d0ffa548a03655b75c3194f747
Rachel Nordgren <rknordgren@gmail.com>
2018-01-28 04:37:31 +0000

    mixregmls_random_mixor.f90 edited online with Bitbucket

--- mixregmls_random_mixor/mixregmls_random_mixor.f90
+++ mixregmls_random_mixor/mixregmls_random_mixor.f90
@@ -1399,7 +1399,7 @@ SUBROUTINE READAT(FILEDAT,NC2,NOBS,MAXK,NVAR,R,P,S,nv,nvar2,Y,X,U,W,var,varavg,t
      end if
         if(discard0 .ne. 0) WRITE(IUN,508)num0
         508 FORMAT(//,1x,'==> The number of level 2 observations removed because of non-varying responses =', I6)
-        if(discard0 .ne. 0 .and. num0 > 0) 
+        if(discard0 .ne. 0 .and. num0 .ge. 1) &
             write(IUN,*) '(see '//trim(fileprefix)//'_removed.dat for information about those clusters)'
 
         CLOSE(IUN)
